44fa85056c62d1a697f82fa9e2e11e7716f784a0
Nerwin Hernando <nerwin.hernando@gobolt.com>
2026-08-01 19:06:54 +0800

    test0(planning): added test and fixtures

--- test/models/plan_test.rb
+++ test/models/plan_test.rb
@@ -1,7 +1,49 @@
-require "test_helper"
+  require "test_helper"
 
 class PlanTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "is valid with valid attributes" do
+    workspace = workspaces(:wedding)
+
+    plan = Plan.new(
+      workspace: workspace,
+      title: "Master Plan"
+    )
+
+    assert plan.valid?
+  end
+
+  test "requires title" do
+    workspace = workspaces(:wedding)
+
+    plan = Plan.new(
+      workspace: workspace
+    )
+
+    assert_not plan.valid?
+    assert_includes plan.errors[:title], "can't be blank"
+  end
+
+  test "belongs to workspace" do
+    plan = plans(:master)
+
+    assert_equal workspaces(:wedding), plan.workspace
+  end
+
+  test "defaults to draft" do
+    workspace = workspaces(:wedding)
+
+    plan = workspace.plans.create!(
+      title: "Planning"
+    )
+
+    assert_equal "draft", plan.status
+  end
+
+  test "can archive" do
+    plan = plans(:master)
+
+    plan.archive!
+
+    assert plan.archived?
+  end
 end
